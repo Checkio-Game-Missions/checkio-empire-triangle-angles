4fd17f24024be9667671350778e427fde2064bfd
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-06-18 16:18:56 +0300

    js update

--- verification/src/referee.py
+++ verification/src/referee.py
@@ -1,5 +1,5 @@
 from checkio_referee import RefereeBase
-from checkio_referee import covercodes, representations
+from checkio_referee import covercodes, representations, ENV_NAME
 
 
 import settings_env
@@ -12,12 +12,10 @@ class Referee(RefereeBase):
 
     DEFAULT_FUNCTION_NAME = "angles"
     ENV_COVERCODE = {
-        "python_2": covercodes.py_unwrap_args,
-        "python_3": covercodes.py_unwrap_args,
-        "javascript": None
+        ENV_NAME.PYTHON: covercodes.py_unwrap_args,
+        ENV_NAME.JS_NODE: covercodes.js_unwrap_args
     }
     CALLED_REPRESENTATIONS = {
-        "python_2": representations.unwrap_arg_representation,
-        "python_3": representations.unwrap_arg_representation,
-        "javascript": representations.unwrap_arg_representation
+        ENV_NAME.PYTHON: representations.unwrap_arg_representation,
+        ENV_NAME.JS_NODE: representations.unwrap_arg_representation
     }
